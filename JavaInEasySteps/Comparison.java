a9145c0fc0008ef1bab6c4258f1cdbcbc11e13f5
LiamTheCoolDude <liam@jonathanmcguinness.com>
2016-11-12 15:12:20 +0100

    Add files via upload

--- JavaInEasySteps/Comparison.java
+++ JavaInEasySteps/Comparison.java
@@ -5,25 +5,42 @@ class Comparison
 	{
 		//Declaration	
 		String	txt,
-		       lang;    	
+		       	lang;    	
 		boolean state;
-
+		int	dozen,
+		  	score;  
 		
+			
 		//Initialisation
 		txt	= "Fantastic ";
 		lang	= "Java";
-
-
+		dozen	= 12;
+		score	= 20;
+		
+		
 		//Function equal to
 		state	= (txt == lang);//Assign test result
-		System.out.println("String Equality Test: " + state);
+		System.out.println("Does Java = Fantastic?");
+		System.out.println("String Equality Test: " + state +"\n");
+		
 
 		//Function not equal to
-		state =(txt != lang);		//Assign result
-		System.out.println("String Inequality Test: "+ state);
+		state	= (txt != lang);		//Assign result
+		System.out.println("Does Java not = Fantastic?");
+		System.out.println("String Inequality Test: " + state +"\n");
+		
+
+		//Function more than
+		state	= (dozen > score);	//Assign result
+		System.out.println("Is a dozen(12) greater than a score(20)?");
+		System.out.println("Greater Than Test: " + state +"\n");
 
 
 
+		//Function less than
+		state	= (dozen < score);	//Assign result
+		System.out.println("Is a dozen(12) less than a score(20)?");
+		System.out.println("Less Than Test: " + state +"\n");
 
 
 
